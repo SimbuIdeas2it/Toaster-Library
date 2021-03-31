470935912e2c5c9335072092903aa65d14ed2d32
Mac <mac@Macs-MacBook-Pro.local>
2021-03-31 19:27:48 +0530

    Static method added

--- toasterlibrary/src/main/java/in/simbu/toasterlibrary/ToasterMessage.kt
+++ toasterlibrary/src/main/java/in/simbu/toasterlibrary/ToasterMessage.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.widget.Toast
 
 class ToasterMessage {
-    fun s(c: Context, message: String) {
-        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    companion object {
+        fun s(c: Context, message: String) {
+            Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+        }
     }
 }
