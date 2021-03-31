cd990f59468a5bc6488bc2f4362b5f72085cb4a1
Mac <mac@Macs-MacBook-Pro.local>
2021-03-31 19:09:01 +0530

    Inital commit

---
+++ toasterlibrary/src/main/java/in/simbu/toasterlibrary/ToasterMessage.kt
@@ -0,0 +1,10 @@
+package `in`.simbu.toasterlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class ToasterMessage {
+    fun s(c: Context, message: String) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
